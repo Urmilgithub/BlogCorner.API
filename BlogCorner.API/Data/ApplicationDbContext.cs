37aeaf1c72e10ffdbee6611ac2055a3c851bf0d2
Urmilgithub <urmilpatelit27@gmail.com>
2024-05-21 13:10:07 +0530

    build: Add Dbcontext class

--- BlogCorner.API/Data/ApplicationDbContext.cs
+++ BlogCorner.API/Data/ApplicationDbContext.cs
@@ -1,6 +1,16 @@
-﻿namespace BlogCorner.API.Data
+﻿using BlogCorner.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogCorner.API.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext: DbContext
     {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+                
+        }
+
+        public DbSet<BlogPost> BlogPosts {  get; set; }
+        public DbSet<Category> categories { get; set; }
     }
 }
