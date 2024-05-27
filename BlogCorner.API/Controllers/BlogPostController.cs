04783342781b8731b755805f27b3bdc916b820b5
Urmilgithub <urmilpatelit27@gmail.com>
2024-05-27 17:34:28 +0530

    feat: add blogpost controller

---
+++ BlogCorner.API/Controllers/BlogPostController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCorner.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogPostController : ControllerBase
+    {
+    }
+}
